9fd3bdca5c4ebd327382a3ef03221e84921ad1ff
ygidtu <ygidtu@gmail.com>
2022-08-09 14:54:28 +0800

    disable debug url

--- web/src/url.js
+++ web/src/url.js
@@ -1,7 +1,7 @@
 export function createURL(...urls) {
     let res = [];
 
-    res.push("http://127.0.0.1:5001")
+    // res.push("http://127.0.0.1:5001")
 
     for (let i=0; i < urls.length; i++) {
         let temp = urls[i].toString();
