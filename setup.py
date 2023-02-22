e4bafd87e0827435e459b4afb552acb41794e9c7
zhou-ran <ranzhou1005@gmail.com>
2023-02-22 10:53:52 +0800

    update hic domain

---
+++ setup.py
@@ -0,0 +1,55 @@
+import os
+from configparser import ConfigParser
+from setuptools import setup, find_packages
+
+__version__ = "0.1.6"
+__author__ = "ygidtu & Ran Zhou"
+__email__ = "ygidtu@gmail.com"
+
+
+def locate_packages():
+    __dir__ = os.path.dirname(os.path.abspath(__file__))
+    pipfile = os.path.join(__dir__, "Pipfile")
+
+    conf = ConfigParser()
+    conf.read(pipfile)
+
+    packages = []
+    for name in conf["packages"]:
+        version = conf.get("packages", name)
+        version = version.strip('"').strip("'")
+        if version == "*":
+            packages.append(name)
+        else:
+            packages.append(f"{name}, {version}")
+    return packages
+
+
+def load_description():
+    __dir__ = os.path.dirname(os.path.abspath(__file__))
+    readme = os.path.join(__dir__, "README.md")
+    if os.path.exists(readme):
+        with open(readme) as r:
+            return r.read()
+    return "README"
+
+
+setup(
+    name='sashimi.py',
+    long_description=load_description(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    url="https://github.com/ygidtu/sashimi.py",
+    entry_points={
+        'console_scripts':
+            [
+                'sashimipy = sashimi.cli:main'
+            ]
+    },
+    python_requires='>=3.7',
+    data_files=[(".", ['README.md', 'Pipfile', 'Pipfile.lock'])],
+    install_requires=locate_packages(),
+    version=__version__
+)
